684392439a33a0f765f9b14e3ac0667f2b5c85fc
Evgeny87 <sun_nik_off@mail.ru>
2026-02-21 00:16:10 +0300

    Create Validator.php

---
+++ src/Validator.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Homework\BracketValidator;
+
+use Homework\BracketValidator\Exceptions\InvalidBracketsException;
+
+class Validator
+{
+    public function isValid(string $input): bool
+    {
+        if (empty(trim($input))) {
+            throw new InvalidBracketsException("String cannot be empty.");
+        }
+
+        $balance = 0;
+        $length = strlen($input);
+
+        for ($i = 0; $i < $length; $i++) {
+            $char = $input[$i];
+
+            if ($char === '(') {
+                $balance++;
+            } elseif ($char === ')') {
+                $balance--;
+            }
+
+            if ($balance < 0) {
+                return false;
+            }
+        }
+
+        return $balance === 0;
+    }
+}
